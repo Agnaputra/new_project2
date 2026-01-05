ae83da02c2c478b480c1fa32db2bdb0ac71cf218
nanaputra <agenaputra@gmail.com>
2026-01-06 05:34:29 +0700

    delete

--- stok-penjualan/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Mengarahkan semua permintaan rute ke app.blade.php agar diproses React Router
-Route::get('/{any?}', function () {
-    return view('app');
-})->where('any', '.*');
