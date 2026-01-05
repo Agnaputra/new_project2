1bcd4296bccfbcc091008072dfd773a05328b2c6
nanaputra <agenaputra@gmail.com>
2026-01-06 04:01:24 +0700

    push all

--- stok-penjualan/resources/js/app.js
+++ stok-penjualan/resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import '../css/app.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App1';
+
+const container = document.getElementById('app');
+
+if (container) {
+    createRoot(container).render(<App />);
+}
---
+++ stok-penjualan/resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stok & Penjualan</title>
+
+    @viteReactRefresh
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
